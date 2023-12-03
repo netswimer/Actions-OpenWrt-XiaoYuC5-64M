2fdde97882463521d61b3b2359c6571d7004122d
netswimer <gyf319@gmail.com>
2023-12-03 14:40:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,8 +11,11 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+  echo 'src-git kenzok8 https://github.com/kenzok8/small-package' >>feeds.conf.default
+  echo 'src-git kenzok8d https://github.com/kenzok8/small' >>feeds.conf.default
+#  echo 'src-git kenzok8 https://github.com/VergilGao/openwrt-packages' >>feeds.conf.default
